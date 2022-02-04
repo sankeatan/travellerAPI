85dd3d7736f6e6820a889a93683818217420c6b3
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-02-04 12:21:57 -0500

    fixed up travellersRoute

--- routes/api/travellersRoute.js
+++ routes/api/travellersRoute.js
@@ -6,37 +6,26 @@ const { Traveller, Location, Trip } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const travellerData = await Traveller.findAll({
-      include: [{ model: LibraryCard }, { model: Book }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              '(SELECT COUNT(*) FROM books WHERE book.pages BETWEEN 100 AND 300 AND book.reader_id = reader.id)'
-            ),
-            'shortBooks',
-          ],
-        ],
-      },
+      include: [{ model: Trip }],
     });
-    res.status(200).json(readerData);
+    res.status(200).json(travellerData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// GET a single reader
+// GET a single traveller
 router.get('/:id', async (req, res) => {
   try {
-    const readerData = await Reader.findByPk(req.params.id, {
-      include: [{ model: LibraryCard }, { model: Book }],
+    const travellerData = await Traveller.findByPk(req.params.id, {
+      include: [{ model: Trip }],
       attributes: {
         include: [
           [
-            // Use plain SQL to add up the total mileage
             sequelize.literal(
-              '(SELECT SUM(mileage) FROM car WHERE car.driver_id = driver.id)'
+              '(SELECT location.name FROM location WHERE trip.location_id = location.id)'
             ),
-            'totalMileage',
+            'location',
           ],
         ],
       },
@@ -53,31 +42,31 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// CREATE a reader
+// CREATE a traveller
 router.post('/', async (req, res) => {
   try {
-    const readerData = await Reader.create(req.body);
+    const travellerData = await Traveller.create(req.body);
     res.status(200).json(readerData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// DELETE a reader
+// DELETE a traveller
 router.delete('/:id', async (req, res) => {
   try {
-    const readerData = await Reader.destroy({
+    const travellerData = await Traveller.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (!readerData) {
-      res.status(404).json({ message: 'No reader found with that id!' });
+    if (!travellerData) {
+      res.status(404).json({ message: 'No traveller found with that id!' });
       return;
     }
 
-    res.status(200).json(readerData);
+    res.status(200).json(travellerData);
   } catch (err) {
     res.status(500).json(err);
   }
